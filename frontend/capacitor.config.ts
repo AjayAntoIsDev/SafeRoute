47e0498421d91a5746ec96fca373a247461ca53c
Ajay Anto <120694977+AjayAntoIsDev@users.noreply.github.com>
2025-07-09 08:22:15 +0000

    added capacitor js

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saferoute.ajay',
+  appName: 'SafeRoute',
+  webDir: 'dist'
+};
+
+export default config;
